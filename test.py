0392dec7a680ecfe5c8159b40ccff7f4df5d2612
arman-rai <011011100111@proton.me>
2025-08-19 10:45:46 +0545

    komit

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
+# V1 
 def sum_of_squares(numbers):
+    sum = 0
     for i in range(0, len(numbers)):
-        print(numbers[i] ** 2)
+        sum +=(numbers[i] ** 2)
+    return sum
 
 numbers = [1, 2, 4, 46]
 
-sum_of_squares(numbers)
+print(sum_of_squares(numbers))
+
+# V2 
+# def sum_of_squares(numbers):
+#     return()
