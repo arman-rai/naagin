66d92d4a8d3b7671395cee5d0275df20417f003f
arman-rai <011011100111@proton.me>
2025-08-19 16:24:37 +0545

    yeah

---
+++ Notes.md
@@ -0,0 +1,7 @@
+Some modules:
+Functional style (map, filter, reduce)
+
+Collections (Counter, deque, defaultdict)
+
+Utility modules (itertools, datetime, json)
+
--- test.py
+++ test.py
@@ -1,9 +1,31 @@
 import math
 from functools import reduce
+from collections import Counter, deque, defaultdict
+from datetime import datetime
+import json
 
-numbers = [1, 5, 54, 12, 66]
-evens = filter(lambda x: x%2 == 0, numbers)
-squares = map(lambda x: math.pow(x,2), evens)
-sum = reduce(lambda a, b: a+b, squares)
+# numbers = [1, 5, 54, 12, 66]
+# evens = filter(lambda x: x%2 == 0, numbers)
+# squares = map(lambda x: math.pow(x,2), evens)
+# sum = reduce(lambda a, b: a+b, squares)
 
-print(sum)
+# print(sum)
+
+word = "mississippi"
+letters = []
+group = defaultdict(list)
+    
+for w in word:
+    group[w[0]].append(w)
+
+print(group)
+print(Counter(word))
+
+data = {
+    "name": "namura",
+    "skills": "none"
+}
+
+print(json.dumps(data))
+print(datetime.now())
+
