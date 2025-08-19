8fffc802e9c9153c2f41b87c905fae7187700959
arman-rai <011011100111@proton.me>
2025-08-19 12:56:49 +0545

    test

--- test.py
+++ test.py
@@ -1,21 +1,9 @@
 import math
+from functools import reduce
 
-# V1 
-# def sum_of_squares(numbers):
-#     sum = 0
-#     for i in range(0, len(numbers)):
-#         sum +=(numbers[i] ** 2)
-#     return sum
+numbers = [1, 5, 54, 12, 66]
+evens = filter(lambda x: x%2 == 0, numbers)
+squares = map(lambda x: math.pow(x,2), evens)
+sum = reduce(lambda a, b: a+b, squares)
 
-# V2 
-# def sum_of_squares(numbers):
-#     return sum(i**2 for i in numbers)
-
-# V3
-# def sum_of_squares(numbers):
-#     return int(sum(math.pow(i,2) for i in numbers))
-
-
-numbers = [1, 2, 4, 46]
-
-print(sum_of_squares(numbers))
+print(sum)
