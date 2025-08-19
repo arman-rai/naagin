005ba4a71c9b4fdf07e817292b9e479516443e90
arman-rai <011011100111@proton.me>
2025-08-19 11:32:39 +0545

    yes

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
+import math
+
 # V1 
-def sum_of_squares(numbers):
-    sum = 0
-    for i in range(0, len(numbers)):
-        sum +=(numbers[i] ** 2)
-    return sum
+# def sum_of_squares(numbers):
+#     sum = 0
+#     for i in range(0, len(numbers)):
+#         sum +=(numbers[i] ** 2)
+#     return sum
+
+# V2 
+# def sum_of_squares(numbers):
+#     return sum(i**2 for i in numbers)
+
+# V3
+# def sum_of_squares(numbers):
+#     return int(sum(math.pow(i,2) for i in numbers))
+
 
 numbers = [1, 2, 4, 46]
 
 print(sum_of_squares(numbers))
-
-# V2 
-# def sum_of_squares(numbers):
-#     return()
